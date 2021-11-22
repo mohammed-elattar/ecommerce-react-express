724429b6144a18dea7092bfc5ff72a2e60facfef
mohammed-elattar <mseel3ttar@gmail.com>
2021-11-22 02:20:43 +0100

    product details screen

--- frontend/src/screens/Product.tsx
+++ frontend/src/screens/Product.tsx
@@ -1,10 +1,81 @@
 import React from 'react';
 import MasterPage from '../components/MasterPage';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import products, { Product } from '../products';
+import Rating from '../components/Rating';
+import { useParams } from 'react-router';
+import { Link } from 'react-router-dom';
 
 interface Props {}
 
-const Product = (props: Props) => {
-  return <MasterPage></MasterPage>;
+const ProductScreen = (props: Props) => {
+  const { id: productId } = useParams();
+  const product = products.find(
+    (product: Product) => product._id === productId
+  );
+
+  return (
+    <MasterPage>
+      <Link className='btn btn-light my-3' to='/'>
+        Go Back
+      </Link>
+      <Row>
+        <Col md='6'>
+          <Image src={product?.image} alt={product?.name} fluid />
+        </Col>
+        <Col md='3'>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>{product?.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product?.rating || 0}
+                text={`${product?.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Price: {product?.price}</ListGroup.Item>
+            <ListGroup.Item>Dription: {product?.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md='3'>
+          <Card>
+            <ListGroup>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>{product?.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {(product?.countInStock || 0) > 0
+                      ? 'In Stock'
+                      : 'Out Of Stock'}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <div className='d-grid gap-2'>
+                  <Button
+                    className='btn-block'
+                    type='button'
+                    disabled={(product?.countInStock || 0) === 0}
+                  >
+                    Add To Cart
+                  </Button>
+                </div>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </MasterPage>
+  );
 };
 
-export default Product;
+export default ProductScreen;
