7120fa8cfac49831466e95eaa09277f60d03be90
mohammed-elattar <mseel3ttar@gmail.com>
2021-11-28 23:07:20 +0100

    auth middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -19,3 +19,19 @@ export const authUser = asyncHandler(async (req, res) => {
   res.status(401);
   throw new Error('invalid user name or password');
 });
+
+export const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    return res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken({ id: user._id }),
+    });
+  }
+
+  res.status(404);
+  throw new Error('user not found');
+});
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import { authUser } from '../controllers/userController.js';
+import { authUser, getUserProfile } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const Router = express.Router();
 Router.post('/login', authUser);
+Router.route('/profile').get(protect, getUserProfile);
 
 export default Router;
