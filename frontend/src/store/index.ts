bfc34bac977f839b02532b8fe5d38764c40b5aea
mohammed-elattar <mseel3ttar@gmail.com>
2021-11-30 23:58:24 +0100

    private router basics

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -2,12 +2,16 @@ import { configureStore } from '@reduxjs/toolkit';
 // import counterReducer from '../features/counter/counter-slice';
 import { apiSlice } from './features/product-api-slice';
 import { cartSlice } from './features/cart-api-slice';
+import authReducer from './features/user-api-slice';
+import { api } from '../services/auth'
 
 export const store = configureStore({
   reducer: {
     // counter: counterReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
+    [api.reducerPath]: api.reducer,
     cart: cartSlice.reducer,
+    auth: authReducer,
   },
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware().concat(apiSlice.middleware);
