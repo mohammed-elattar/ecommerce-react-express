e7371699934a8f2ce58b567e25689161d118af80
mohammed-elattar <mseel3ttar@gmail.com>
2021-12-13 00:35:57 +0100

    BE - Create & update product endpoints

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -27,4 +27,60 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
-export { listProducts, getProductById, deleteProduct };
+const createProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    image,
+    brand,
+    category,
+    countInStock,
+    numReviews,
+    description,
+  } = req.body;
+  const product = new Product({
+    name,
+    price,
+    user: req.user._id,
+    image,
+    brand,
+    category,
+    countInStock,
+    numReviews,
+    description,
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
+export {
+  listProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import {
+  createProduct,
   deleteProduct,
   getProductById,
   listProducts,
+  updateProduct,
 } from '../controllers/productController.js';
 import { admin, protect } from '../middleware/authMiddleware.js';
 
 const Router = express.Router();
-Router.get('/', listProducts);
-Router.get('/:id', getProductById);
-Router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct);
+Router.route('/').get(listProducts).post(protect, admin, createProduct);
+Router.route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 export default Router;
