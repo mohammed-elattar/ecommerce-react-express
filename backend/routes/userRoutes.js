811ae5ffde5f447b4916a4a17d4e6b932d33af5f
mohammed-elattar <mseel3ttar@gmail.com>
2021-12-12 01:08:20 +0100

    BE - admin can delete user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,15 +5,17 @@ import {
   getUsers,
   register,
   updateUserProfile,
+  deleteUser,
 } from '../controllers/userController.js';
 import { admin, protect } from '../middleware/authMiddleware.js';
 
 const Router = express.Router();
 Router.post('/login', authUser);
-Router.route('/').get(protect, admin, getUsers);
 Router.post('/register', register);
 Router.route('/profile')
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
+Router.route('/').get(protect, admin, getUsers);
+Router.route('/:id').delete(protect, admin, deleteUser);
 
 export default Router;
