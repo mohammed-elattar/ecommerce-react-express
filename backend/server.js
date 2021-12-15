28f3ea87df1635c3b220855de83cd5ddc22cddea
mohammed-elattar <mseel3ttar@gmail.com>
2021-12-15 23:45:33 +0100

    BE - create review api

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import morgan from 'morgan';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -12,6 +13,11 @@ const app = express();
 dotenv.config();
 app.use(express.json());
 connectDB();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 const port = process.env.PORT || 3005;
 app.get('/', (req, res) => {
   res.send('Hello World!');
